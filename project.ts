764acd5d2705681cce1db19d40942f2a8ff504f3
Steve Gill <stevengill97@gmail.com>
2022-01-26 14:44:00 -0800

    removed function and workflow registration from project.ts, updated to sdk version 1.1.0

--- project.ts
+++ project.ts
@@ -1,8 +1,5 @@
 import { Project } from "slack-cloud-sdk/mod.ts";
-import { ReceiveMetadataEventWorkflow } from "./workflows/receive_metadata_event.ts";
 import { ReceiveMetadataEvent } from "./triggers/receive_metadata_event.ts";
-import { SendMetadataEventFunction } from "./functions/send_metadata_event.ts";
-import { SendMetadataEventWorkflow } from "./workflows/send_metadata_event.ts";
 import { SendMetadataEventShortcut } from "./triggers/send_metadata_event_shortcut.ts";
 
 Project({
@@ -12,8 +9,6 @@ Project({
   icon: "assets/icon.png",
   runtime: "deno1.x",
   botScopes: ["commands", "chat:write", "chat:write.public"],
-  functions: [SendMetadataEventFunction],
-  workflows: [ReceiveMetadataEventWorkflow, SendMetadataEventWorkflow],
   triggers: [ReceiveMetadataEvent, SendMetadataEventShortcut],
   tables: [],
   outgoingDomains: [],
